475cfaf589e46aba8e141eca4b4245a5ad937e70
tiqwab <tiqwab.ch90@gmail.com>
2017-04-24 15:33:33 +0900

    Calculate simple expression

--- src/main/java/com/tiqwab/example/ParserMain.java
+++ src/main/java/com/tiqwab/example/ParserMain.java
@@ -4,6 +4,6 @@ public class ParserMain {
 
     public static void main(String[] args) throws Exception {
         Parser parser = new Parser(System.in);
-        parser.start();
+        System.out.println(parser.start());
     }
 }
