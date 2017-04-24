e8ae871facce9066fe582c2f269746fa15c1a125
tiqwab <tiqwab.ch90@gmail.com>
2017-04-24 14:15:55 +0900

    Parse number

--- src/main/java/com/tiqwab/example/ParserMain.java
+++ src/main/java/com/tiqwab/example/ParserMain.java
@@ -4,6 +4,6 @@ public class ParserMain {
 
     public static void main(String[] args) throws Exception {
         Parser parser = new Parser(System.in);
-        parser.List();
+        parser.number();
     }
 }
