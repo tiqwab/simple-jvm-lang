a51a6fde10983eae2d2e5371675bcff998c98645
tiqwab <tiqwab.ch90@gmail.com>
2017-04-24 13:47:54 +0900

    Generate a very simple parser

---
+++ src/main/java/com/tiqwab/example/ParserMain.java
@@ -0,0 +1,9 @@
+package com.tiqwab.example;
+
+public class ParserMain {
+
+    public static void main(String[] args) throws Exception {
+        Parser parser = new Parser(System.in);
+        parser.List();
+    }
+}
