b01c127068bb90b28a3a5033f263d0ef6115d16b
tiqwab <tiqwab.ch90@gmail.com>
2017-04-26 15:52:55 +0900

    All ast classes extends ASTNodeBase

---
+++ src/main/java/com/tiqwab/example/ASTNodeBase.java
@@ -0,0 +1,13 @@
+package com.tiqwab.example;
+
+public class ASTNodeBase extends SimpleNode {
+
+    public ASTNodeBase(Parser p, int i) {
+        super(p, i);
+    }
+
+    public ASTNodeBase(int i) {
+        super(i);
+    }
+
+}
