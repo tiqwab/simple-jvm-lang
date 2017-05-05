1973f566ad7b0bc47e101401b6bfc23aed0f6476
tiqwab <tiqwab.ch90@gmail.com>
2017-05-05 14:16:54 +0900

    Calculate LT of int values

--- src/main/java/com/tiqwab/example/jbc/JBCRelOperator.java
+++ src/main/java/com/tiqwab/example/jbc/JBCRelOperator.java
@@ -2,7 +2,9 @@ package com.tiqwab.example.jbc;
 
 import com.tiqwab.example.Environment;
 import com.tiqwab.example.symbol.Type;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
 public class JBCRelOperator extends JBCExprBase {
 
@@ -29,6 +31,21 @@ public class JBCRelOperator extends JBCExprBase {
 
     @Override
     public void genCode(MethodVisitor mv, Environment env) {
+        Type actualType = Type.max(lhs.getType(env), rhs.getType(env)).orElseThrow(
+                () -> new IllegalStateException(String.format("Cannot apply operation. lhs: %s, op: %s, rhs: %s", lhs, op, rhs))
+        );
+        this.lhs.setWidenedType(actualType);
+        this.lhs.genCode(mv, env);
+        this.rhs.setWidenedType(actualType);
+        this.rhs.genCode(mv, env);
 
+        Label labelTrue = new Label();
+        Label labelFalse = new Label();
+        mv.visitJumpInsn(Opcodes.IF_ICMPLT, labelTrue);
+        mv.visitInsn(Opcodes.ICONST_0);
+        mv.visitJumpInsn(Opcodes.GOTO, labelFalse);
+        mv.visitLabel(labelTrue);
+        mv.visitInsn(Opcodes.ICONST_1);
+        mv.visitLabel(labelFalse);
     }
 }
