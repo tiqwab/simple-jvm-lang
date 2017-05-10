dc4fc87ba64da1a4b51d635ffbd189db6804db4d
tiqwab <tiqwab.ch90@gmail.com>
2017-05-10 18:15:17 +0900

    Run from gradle

--- src/main/java/com/tiqwab/example/ParserMain.java
+++ src/main/java/com/tiqwab/example/ParserMain.java
@@ -6,18 +6,11 @@ import com.tiqwab.example.jbc.JBCNode;
 public class ParserMain {
 
     public static void main(String[] args) throws Exception {
+        System.out.println();
+
         Parser parser = new Parser(System.in);
         SimpleNode node = parser.Start();
 
-        System.out.println("Dump...");
-        node.dump("");
-        System.out.println();
-
-        System.out.println("Visit...");
-        node.jjtAccept(new SampleVisitor(), null);
-        System.out.println();
-
-        System.out.println("Generate java bytecodes...");
         ASTNodeBase astNode = (ASTNodeBase) node;
         JBCNode jbcNode = astNode.getJbcNode();
         System.out.println(jbcNode.toString());
